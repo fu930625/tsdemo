5928eeed9fe2353420c3758485beeb8f700022b7
futingting <futingting@huatu.com>
2023-12-28 13:20:34 +0800

    代码

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {}
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
